dee3a624cbba40c449d35f0f16fe722254ea4f43
Felipe Soares <86936050+felipesoarws@users.noreply.github.com>
2022-08-17 15:45:14 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// navegation menu
+
 const navLis = document.querySelectorAll("nav li");
 
 navLis.forEach((li) => {
@@ -45,3 +47,81 @@ window.addEventListener("scroll", () => {
     desktopLis[0].classList.add("active");
   }
 });
+
+// navegation gallery
+
+const slides = document.querySelectorAll(".gallery .slider img");
+const mainSlide = document.querySelector(".gallery .bg");
+
+slides.forEach((slide) => {
+  slide.addEventListener("click", () => {
+    resetSlides(slides, "selected");
+
+    const img = slide.src.split("images/")[1];
+    mainSlide.style.background = `url(../images/${img})`;
+    mainSlide.style.backgroundSize = `cover`;
+    mainSlide.style.backgroundRepeat = `no-repeat`;
+    mainSlide.style.backgroundPosition = `0rem`;
+
+    slide.classList.add("selected");
+  });
+});
+
+const resetSlides = (item) => {
+  item.forEach((i) => {
+    i.classList.remove("selected");
+  });
+};
+
+// navegation video
+
+const processor = {
+  timerCallback() {
+    if (this.video.paused || this.video.ended) {
+      return;
+    }
+    this.computeFrame();
+    setTimeout(() => {
+      this.timerCallback();
+    }, 16); // roughly 60 frames per second
+  },
+
+  doLoad() {
+    this.video = document.getElementById("my-video");
+    this.c1 = document.getElementById("my-canvas");
+    this.ctx1 = this.c1.getContext("2d");
+
+    this.video.addEventListener(
+      "play",
+      () => {
+        this.width = this.video.width;
+        this.height = this.video.height;
+        this.timerCallback();
+      },
+      false
+    );
+  },
+
+  computeFrame() {
+    this.ctx1.drawImage(this.video, 0, 0, this.width, this.height);
+    const frame = this.ctx1.getImageData(0, 0, this.width, this.height);
+    const l = frame.data.length / 4;
+
+    for (let i = 0; i < l; i++) {
+      const grey =
+        (frame.data[i * 4 + 0] +
+          frame.data[i * 4 + 1] +
+          frame.data[i * 4 + 2]) /
+        3;
+
+      frame.data[i * 4 + 0] = grey;
+      frame.data[i * 4 + 1] = grey;
+      frame.data[i * 4 + 2] = grey;
+    }
+    this.ctx1.putImageData(frame, 0, 0);
+
+    return;
+  },
+};
+
+processor.doLoad();
