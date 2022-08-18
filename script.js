b8cb8bead76f5d6241ab1f7832b5729d931b69e5
Felipe Soares <86936050+felipesoarws@users.noreply.github.com>
2022-08-18 11:24:43 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,31 +1,15 @@
-// navegation menu
+// menu navegation
 
 const navLis = document.querySelectorAll("nav li");
 
 navLis.forEach((li) => {
   li.addEventListener("click", () => {
-    resetClasses(navLis);
+    resetClasses(navLis, "active");
     addClass(li, "active");
   });
 });
 
-const removeClass = (item, classToRemove) => {
-  item.classList.remove(classToRemove);
-};
-
-const addClass = (item, classToAdd) => {
-  item.classList.add(classToAdd);
-};
-
-const resetClasses = (item) => {
-  item.forEach((i) => {
-    i.classList.remove("active");
-  });
-};
-
-
-
-// navegation gallery
+// gallery navegation
 
 const slides = document.querySelectorAll(".gallery .slider img");
 const mainSlide = document.querySelector(".gallery .bg");
@@ -50,4 +34,78 @@ const resetSlides = (item) => {
   });
 };
 
+// cast navegation
 
+const cards = document.querySelectorAll(".content-characters .card");
+const characterImage = document.querySelector(".characters-image img");
+const cardsDescriptions = document.querySelectorAll(".content-desc .desc");
+
+cards.forEach((card) => {
+  card.addEventListener("click", () => {
+    console.log(card.classList[1]);
+    console.log(cardsDescriptions);
+    switch (card.classList[1]) {
+      case "kratos":
+        changeCard(
+          characterImage,
+          "images/characters/kratos.png",
+          cardsDescriptions[0],
+          card
+        );
+        break;
+      case "atreus":
+        changeCard(
+          characterImage,
+          "images/characters/atreus.png",
+          cardsDescriptions[1],
+          card
+        );
+        break;
+
+      case "angrboda":
+        changeCard(
+          characterImage,
+          "images/characters/angrboda.png",
+          cardsDescriptions[2],
+          card
+        );
+        break;
+
+      case "freya":
+        changeCard(
+          characterImage,
+          "images/characters/freya.png",
+          cardsDescriptions[3],
+          card
+        );
+        break;
+
+      default:
+        break;
+    }
+  });
+});
+
+const changeCard = (img, source, desc, card) => {
+  img.src = source;
+  resetClasses(cardsDescriptions, "visible");
+  resetClasses(cards, "active");
+  addClass(card, "active");
+  addClass(desc, "visible");
+};
+
+// geral functions
+
+const removeClass = (item, classToRemove) => {
+  item.classList.remove(classToRemove);
+};
+
+const addClass = (item, classToAdd) => {
+  item.classList.add(classToAdd);
+};
+
+const resetClasses = (item, act) => {
+  item.forEach((i) => {
+    i.classList.remove(act);
+  });
+};
